aeb3c565a2b37f121ae3f50f2886fd73f76aab93
Varun Mathur <97465624+vrunm@users.noreply.github.com>
2023-01-05 10:53:39 +0530

    Update 3.summarization_finetuned.py

--- 3.summarization_finetuned.py
+++ 3.summarization_finetuned.py
@@ -83,18 +83,6 @@ def compute_metrics(eval_pred):
     predictions, labels = eval_pred
     decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
 
-    # Replace -100 in the labels as we can't decode them.
-    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-
-    # Rouge expects a newline after each sentence
-    decoded_preds = [
-        "\n".join(nltk.sent_tokenize(pred.strip())) for pred in decoded_preds
-    ]
-    decoded_labels = [
-        "\n".join(nltk.sent_tokenize(label.strip())) for label in decoded_labels
-    ]
-
     result = metric.compute(
         predictions=decoded_preds, references=decoded_labels, use_stemmer=True
     )
